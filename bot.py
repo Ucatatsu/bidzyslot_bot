d29afb05ee3e4e4dbf9061bde7d0572860ffec0c
Ucatatsu <hajemuru@hotmail.com>
2025-10-07 18:27:58 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 # Укажи свой токен бота
 BOT_TOKEN = "8234769062:AAHtki4WnMnvN31hDVYWO55fD6lMxmYCYQU"
 # URL где будет размещено веб-приложение
-WEB_APP_URL = "https://bidzyslot-bot.vercel.app/"
+WEB_APP_URL = "https://ucatatsu.github.io/bidzyslot_bot/"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Отправляет приветственное сообщение с кнопкой для открытия веб-приложения"""
